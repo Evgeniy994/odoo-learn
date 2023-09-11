e3eb58603175534e17d2c36dd6f9fbcaaa448123
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2023-09-11 19:49:22 +0300

    Add files via upload

---
+++ estate/estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+	'name': "Learn",
+	'version': '1.0',
+	'depends': ['base'],
+	'author': "Me",
+	'category': 'Category',
+	'installable': True,
+	'description': """
+	Some interesting text!
+	""",
+	'data': [
+		'security/ir.model.access.csv',
+		'views/estate_property_views.xml',
+		'views/estate_property_type_views.xml',
+		'views/estate_property_tag_views.xml',
+		'views/estate_property_offer_views.xml',
+		'views/estate_menus.xml',
+	]
+}
