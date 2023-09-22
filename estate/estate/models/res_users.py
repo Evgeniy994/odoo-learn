db7a962504101bd14a6c18310ce2c063c022a936
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2023-09-22 21:12:46 +0300

    Delete estate directory

--- estate/estate/models/res_users.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class ResUsersProperty(models.Model):
-    _inherit = 'res.users'
-
-    property_ids = fields.One2Many('estate.property', 'salesman_id', string='Properties')
